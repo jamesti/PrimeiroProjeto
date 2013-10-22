cbd26d902768a1a4c96fa5b67f9eb96f05e0f9ff
James Moreira de Sousa Junior <jamesmsjr@gmail.com>
2013-10-22 10:55:46 -0300

    Tela 2 e Eventos Activityes

--- gen/com/example/primeiroprojeto/R.java
+++ gen/com/example/primeiroprojeto/R.java
@@ -24,12 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int button1=0x7f080007;
+        public static final int buttonProximo=0x7f080003;
+        public static final int editTextNome=0x7f080001;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080005;
+        public static final int textViewMensagem=0x7f080000;
+        public static final int textViewResposta=0x7f080002;
     }
     public static final class layout {
         public static final int activity_tela1=0x7f030000;
+        public static final int activity_tela2=0x7f030001;
     }
     public static final class menu {
         public static final int tela1=0x7f070000;
@@ -38,8 +45,16 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int aluno=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int botao_ok=0x7f050008;
+        public static final int botao_proximo=0x7f050007;
+        public static final int botao_voltar=0x7f05000c;
         public static final int curso=0x7f050004;
+        public static final int ensino=0x7f05000a;
         public static final int hello_world=0x7f050002;
+        public static final int msg=0x7f050005;
+        public static final int resposta=0x7f050009;
+        public static final int texto_nome=0x7f050006;
+        public static final int titulo=0x7f05000b;
     }
     public static final class style {
         /** 
--- src/com/example/primeiroprojeto/Tela1.java
+++ src/com/example/primeiroprojeto/Tela1.java
@@ -2,14 +2,23 @@ package com.example.primeiroprojeto;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Tela1 extends Activity {
+public class Tela1 extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela1);
+        
+        Button btn = (Button)findViewById(R.id.buttonProximo);
+        btn.setOnClickListener(this);
     }
 
 
@@ -19,5 +28,20 @@ public class Tela1 extends Activity {
         getMenuInflater().inflate(R.menu.tela1, menu);
         return true;
     }
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		TextView tvResultado = (TextView) findViewById(R.id.textViewResposta);
+		EditText txtNome = (EditText) findViewById(R.id.editTextNome);
+		  
+		String msg = "Nome: " + txtNome.getText().toString();
+		  
+		tvResultado.setText(msg);
+		
+		Intent intencao = new Intent(this, Tela2.class);
+		startActivity(intencao);
+	}
     
 }
